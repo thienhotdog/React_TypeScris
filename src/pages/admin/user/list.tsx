fdd089b0a4cf913f066b06b7452ada6957d7ce22
QuangHieu <thienhotdog@gmail.com>
2021-12-12 04:10:14 +0700

    update

--- src/pages/admin/user/list.tsx
+++ src/pages/admin/user/list.tsx
@@ -65,11 +65,6 @@ const ListUser: React.FC = () => {
         <div>
             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h2 className="h2">Quản lý User</h2>
-                <div className="btn-toolbar mb-2 mb-md-0">
-                    <Link to="/admin/addproduct" className="btn btn-sm btn-outline-primary">
-                        Thêm sản phẩm
-                    </Link>
-                </div>
             </div>
             <div className="table-responsive">
 
